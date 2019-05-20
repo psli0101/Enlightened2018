ddff87aa16f2d3020dc4aa876bcdbe1cb65475fa
ps lee <ps.lee@tenoz.tw>
2019-05-20 22:15:41 +0800

    upload

---
+++ sample.js
@@ -0,0 +1,26 @@
+var s1 = "string";
+var s2 = 'string';
+
+var num = 123;
+
+var boo1 = true;
+var boo2 = false;
+
+var array1 = [1, 2, 3];
+var array2 = ['A', 'B', 'C'];
+
+var object = {
+    "name":"Cat",
+    "age":2,
+    "favorite":"PS"
+}
+
+var x = null;
+var y ;
+
+function square(number) {
+    return number * number;
+}
+var square = function(number) {
+    return number * number;
+}
---
+++ test.js
@@ -0,0 +1,4 @@
+function ClickMe2(){
+    alert("無事退朝。");
+    console.log(123456);
+}
